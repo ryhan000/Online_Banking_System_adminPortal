7e81c96e7b9975ad8d545592e7885a57fc6788e6
ryhan000 <rayhan1ahamed@gmail.com>
2018-04-09 01:39:42 +0600

    Added login component and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-//import { routing }  from './app.routing';
+import { routing }  from './app.routing';
 
 
 import { AppComponent } from './app.component';
 import {NabvarComponent} from './nabvar/nabvar.component';
+import { LoginComponent } from './login/login.component';
 
+import { LoginService } from './login.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NabvarComponent
+    NabvarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ LoginService,],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
