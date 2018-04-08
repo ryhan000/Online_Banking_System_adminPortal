09ec0965d20c64dd1510a14765e673410cfec6c2
ryhan000 <rayhan1ahamed@gmail.com>
2018-04-08 23:56:26 +0600

    Added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+//import { routing }  from './app.routing';
 
 
 import { AppComponent } from './app.component';
+import {NabvarComponent} from './nabvar/nabvar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NabvarComponent
   ],
   imports: [
     BrowserModule
@@ -16,3 +21,4 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ src/app/nabvar/nabvar.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-nabvar',
+  templateUrl: './nabvar.component.html'
+})
+export class NabvarComponent implements OnInit {
+
+ 
+
+	constructor() {
+	
+	}
+
+
+  ngOnInit() {
+  }
+
+}
